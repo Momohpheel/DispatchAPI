d66312ae0adb59020e58cf5cfd9373d36ec18ee8
Momohpheel <momohmayowa14@gmail.com>
2021-08-20 13:35:25 +0100

    cron jobs update paymeclear

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -428,6 +428,12 @@ class UserRepository implements UserRepositoryInterface{
 
                     }
 
+                   function pxm($a, $b) {
+                        if ($a['distance']==$b['distance']) return 0;
+                            return ($a['distance']<$b['distance'])?-1:1;
+                        //return $a['distance'] > $b['distance'];
+                    }
+
                     uasort($final_riders_proximity, 'App\Repositories\pxm');
 
                     $closest_rider = array();
