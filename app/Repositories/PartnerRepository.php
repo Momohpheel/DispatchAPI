a8c83be5a0ea12efc8e4706a9a7b7c37104d39c1
Momohpheel <momohmayowa14@gmail.com>
2021-09-10 11:52:52 +0100

    paginate

--- app/Repositories/PartnerRepository.php
+++ app/Repositories/PartnerRepository.php
@@ -1035,7 +1035,7 @@ class PartnerRepository implements PartnerRepositoryInterface{
         try{
 
 
-            $orders = Order::with('dropoff')->where('partner_id', auth()->user()->id)->paginate();
+            $orders = Order::with('dropoff')->where('partner_id', auth()->user()->id)->paginate(10);
             $data = [];
 
 
