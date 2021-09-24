e44e04099f86476fab3b862e9cce1c60bb81647c
Momohpheel <momohmayowa14@gmail.com>
2021-09-24 10:18:44 +0100

    route cost

--- app/Repositories/PartnerRepository.php
+++ app/Repositories/PartnerRepository.php
@@ -1096,9 +1096,11 @@ class PartnerRepository implements PartnerRepositoryInterface{
     public function getRouteCost(){
         try{
             $route_costing = RouteCosting::where('partner_id', auth()->user()->id)->first();
-
+            if ($route_costing){
                 return $this->success(false, "Route-Costing", $route_costing,200);
-
+            }else{
+                return $this->error(true, "No route cost found", 400);
+            }
         }catch(Exception $e){
             return $this->error(true, "Error occured", 400);
         }
